e8cdb9add54748f1dd4e118001d554c055723454
Andrew Leitsius <leits@quintagroup.com>
2018-06-01 09:54:38 +0300

    Add dummy test

---
+++ schematics_flexible/tests/dummy_test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+class DummyTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
